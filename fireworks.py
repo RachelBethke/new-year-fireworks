89fbbd79bcb2535d55bd7706e5ea565dd604eebb
RachelBethke <rkb76@cornell.edu>
2025-01-01 03:03:52 -0500

    Upgraded to shoot the fireworks out of rockets and get rid of the cursor being visible

--- fireworks.py
+++ fireworks.py
@@ -3,26 +3,33 @@ import time
 import random
 
 def draw_fireworks(stdscr):
+    curses.curs_set(0)
     curses.start_color()
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-    
+
     stdscr.clear()
     height, width = stdscr.getmaxyx()
 
-    for _ in range(50): #firewokrs
+    for _ in range(10): #firewokrs
         x = random.randint(1, width - 2)
-        y = random.randint(1, height - 2)
+        explosion_height = random.randint(height // 3, height - 5)
 
-        #explosion animations
-        for _ in range(5): #time
+        # upwards animation
+        for y in range(height - 2, explosion_height, -1):
+            stdscr.clear()
+            stdscr.addstr(y, x, "|", curses.color_pair(random.randint(1, 5)))
+            stdscr.refresh()
+            time.sleep(0.05)
+
+        for _ in range(5):  # duration
             color = random.randint(1, 5)
             char = random.choice(["*", "o", "+", "."])
             for dx, dy in [(-1, -1), (-1, 1), (1, -1), (1, 1), (0, 0)]:
-                nx, ny = x + dx, y + dy
+                nx, ny = x + dx, explosion_height + dy
                 if 0 < ny < height and 0 < nx < width:
                     stdscr.addstr(ny, nx, char, curses.color_pair(color))
             stdscr.refresh()
